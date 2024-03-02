ad245bb436c3f2e393ad8fc7c1d6b3a38b16c671
itsSoheil <soheil.samadi79@gmail.com>
2024-03-02 21:32:45 +0330

    Create CheckOTPForms component

---
+++ src/features/authentication/CheckOTPForms.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import OTPInput from "react-otp-input";
+
+export default function CheckOTPForms() {
+  const [otp, setOtp] = useState("");
+  return (
+    <div>
+      <form className="space-y-4">
+        <p className="font-VazirBold text-secondary-800">کد تایید را وارد کنید</p>
+        <OTPInput
+          value={otp}
+          onChange={setOtp}
+          numInputs={6}
+          renderSeparator={<span></span>}
+          renderInput={(props) => <input type="number" {...props} />}
+          containerStyle="flex flex-row-reverse gap-x-2 justify-center"
+          inputStyle={{
+            width: "2.5rem",
+            padding: "0.5rem 0.2rem",
+            border: "1px solid #b7c5ff",
+            borderRadius: "0.5rem",
+            backgroundColor: "white",
+          }}
+        />
+        <button className="btn btn--primery w-full">تایید</button>
+      </form>
+    </div>
+  );
+}
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,10 +1,13 @@
+import CheckOTPForms from "../features/authentication/CheckOTPForms";
 import SendOTPForm from "../features/authentication/SendOTPForm";
 
 export default function Auth() {
   return (
-    <div className="w-full sm:max-w-sm">
-      <h1>این صفحه ی Auth هست</h1>
-      <SendOTPForm />
+    <div className="w-ful h-screen flex justify-center items-center">
+      <div className="w-full sm:max-w-sm space-y-6">
+        <SendOTPForm />
+        <CheckOTPForms />
+      </div>
     </div>
   );
 }
