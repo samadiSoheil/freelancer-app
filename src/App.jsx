689b355e4a268bc3027ea0ff5e696fb822dba2d4
itsSoheil <soheil.samadi79@gmail.com>
2024-03-07 09:48:05 +0330

    Add QueryClient and its Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Auth from "./pages/Auth";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <div className="container">
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </QueryClientProvider>
     </div>
   );
 }
