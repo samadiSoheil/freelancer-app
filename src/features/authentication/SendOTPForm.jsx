677a4c43f33e6309346eef5ecc97fc870128b8b6
itsSoheil <soheil.samadi79@gmail.com>
2024-03-07 09:56:20 +0330

    Send number with react-query for Otp

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -1,18 +1,49 @@
 import { useState } from "react";
 import TextFiled from "../../ui/TextField";
+import { useMutation } from "@tanstack/react-query";
+import { getOtp } from "../../services/authServise";
+import toast from "react-hot-toast";
+import Loading from "../../ui/Loading";
 
-export default function SendOTPForm() {
+export default function SendOTPForm({ onChamgeStep }) {
   const [phoneNumber, setPhoneNumber] = useState("");
 
+  const { isPending, error, data, mutateAsync } = useMutation({
+    mutationFn: getOtp,
+  });
+
+  const senfOtpHandler = async (e) => {
+    e.preventDefault();
+    console.log(phoneNumber);
+    try {
+      const { data } = await mutateAsync({ phoneNumber });
+      console.log(data);
+      toast.success(data.message);
+      onChamgeStep(2);
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    }
+    setPhoneNumber("");
+  };
+
   return (
-    <form className="space-y-4">
+    <form className="space-y-4" onSubmit={senfOtpHandler}>
       <TextFiled
         name="phoneNumber"
         label="شماره موبایل"
         value={phoneNumber}
         onChange={(e) => setPhoneNumber(e.target.value)}
       />
-      <button className="btn btn--primery w-full">ارسال کد تایید</button>
+      <div>
+        {!isPending ? (
+          <Loading width={60} />
+        ) : (
+          <button type="submit" className="btn btn--primery w-full">
+            ارسال کد تایید
+          </button>
+        )}
+      </div>
     </form>
   );
 }
