a42e580abc622f3b522b0d77b835d8c9b853a916
itsSoheil <soheil.samadi79@gmail.com>
2024-03-09 12:01:18 +0330

    Some changes

--- src/ui/TextField.jsx
+++ src/ui/TextField.jsx
@@ -1,15 +1,15 @@
-export default function TextFiled({ label, name, value, onChange }) {
+export default function TextFiled({ label, name, value, onChange, type }) {
   return (
     <div>
-      <label htmlFor={name} className="block mb-4 font-VazirMedium">
+      <label htmlFor={name} className="block mb-4 font-VazirMedium  text-secondary-900">
         {label}
       </label>
       <input
         id={name}
         value={value}
         onChange={onChange}
-        type="text"
-        className="textFiled__input"
+        type={type}
+        className="textFiled__input w-full"
         autoComplete="off"
       />
     </div>
