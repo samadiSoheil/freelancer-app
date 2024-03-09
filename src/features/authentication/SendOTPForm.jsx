9199e91513bc59d6628c66242a2610787c263f2c
itsSoheil <soheil.samadi79@gmail.com>
2024-03-09 11:46:42 +0330

    Transfer senfOtpHandler  to its father's level

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -1,42 +1,18 @@
-import { useState } from "react";
 import TextFiled from "../../ui/TextField";
-import { useMutation } from "@tanstack/react-query";
-import { getOtp } from "../../services/authServise";
-import toast from "react-hot-toast";
 import Loading from "../../ui/Loading";
 
-export default function SendOTPForm({ onChamgeStep }) {
-  const [phoneNumber, setPhoneNumber] = useState("");
-
-  const { isPending, error, data, mutateAsync } = useMutation({
-    mutationFn: getOtp,
-  });
-
-  const senfOtpHandler = async (e) => {
-    e.preventDefault();
-    console.log(phoneNumber);
-    try {
-      const { data } = await mutateAsync({ phoneNumber });
-      console.log(data);
-      toast.success(data.message);
-      onChamgeStep(2);
-    } catch (err) {
-      console.log(err);
-      toast.error(err.message);
-    }
-    setPhoneNumber("");
-  };
-
+export default function SendOTPForm({ setPhoneNumber, phoneNumber, sendOtp, isPending }) {
   return (
-    <form className="space-y-4" onSubmit={senfOtpHandler}>
+    <form className="space-y-4" onSubmit={sendOtp}>
       <TextFiled
         name="phoneNumber"
         label="شماره موبایل"
+        type="number"
         value={phoneNumber}
         onChange={(e) => setPhoneNumber(e.target.value)}
       />
       <div>
-        {!isPending ? (
+        {isPending ? (
           <Loading width={60} />
         ) : (
           <button type="submit" className="btn btn--primery w-full">
