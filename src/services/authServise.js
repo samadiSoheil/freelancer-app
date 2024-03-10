691eaec3cfb700ab96c7a37477dbc6556283e069
itsSoheil <soheil.samadi79@gmail.com>
2024-03-10 10:09:37 +0330

    Create completeProfileFunc service for complete user profile

--- src/services/authServise.js
+++ src/services/authServise.js
@@ -7,3 +7,7 @@ export function getOtp(data) {
 export function checkOtp(data) {
   return http.post("/user/check-otp", data).then((res) => res.data.data);
 }
+
+export function completeProfileFunc(data) {
+  return http.post("/user/complete-profile", data).then((res) => res.data.data);
+}
