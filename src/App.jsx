e77a726a3ab88121286cc306c51f8a07d3318e2b
itsSoheil <soheil.samadi79@gmail.com>
2024-03-10 10:22:38 +0330

    Create and add NotFound component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import CompleteProfile from "./pages/CompleteProfile";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   const queryClient = new QueryClient();
@@ -17,6 +18,7 @@ function App() {
           <Route path="/auth" element={<Auth />} />
           <Route path="/complete-profile" element={<CompleteProfile />} />
           <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </QueryClientProvider>
     </div>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <>
+      <h1>this is NotFound</h1>
+    </>
+  );
+}
